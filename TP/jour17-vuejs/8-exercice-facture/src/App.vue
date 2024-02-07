4917a6add3284678e91b0f1bad7b7f9ac32e3385
Jérôme Poslednik <webmaster@washaweb.com>
2024-02-07 10:48:10 +0100

    correction exercice bill partie 1

--- TP/jour17-vuejs/8-exercice-facture/src/App.vue
+++ TP/jour17-vuejs/8-exercice-facture/src/App.vue
@@ -1,21 +1,47 @@
 <template>
   <main>
-    <div class="container">
-      <h1 class="my-4">Bienvenue : Vue avec Bootstrap et fontawesome</h1>
-      <p class="lead">
-        Icône Fontawesome: <br />
-        <i class="fa-regular fa-user"></i> <i class="fa-solid fa-lock"></i><br />
-        <i class="fa-brands fa-youtube"></i> <i class="fa-brands fa-facebook"></i>
-      </p>
-      <p>
-        <button class="btn btn-primary">I'm Good!</button>
-      </p>
-      <p>
-        <button class="btn btn-secondary">Me too!</button>
-      </p>
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-md-9">
+          <h1 class="h2 mb-4"><i class="fa-solid fa-angle-down me-2 fs-3" />Liste des factures</h1>
+        </div>
+        <div class="col-md-3 text-end">
+          <button class="btn btn-outline-info" type="button">
+            <i class="fa-solid fa-circle-plus me-1" />
+            Créer une facture
+          </button>
+        </div>
+      </div>
+
+      <div class="border rounded-3 p-2">
+        <table class="table align-middle mb-0">
+          <tbody>
+            <BillTableRow v-for="bill in bills" :key="bill.id" :bill="bill" />
+          </tbody>
+        </table>
+      </div>
+      <!-- <pre>
+        {{ bills }}
+      </pre> -->
     </div>
   </main>
 </template>
+
+<script>
+import BillTableRow from '@/components/BillTableRow.vue'
+import { bills } from '@/seeds/bills.js'
+
+export default {
+  components: {
+    BillTableRow
+  },
+  data() {
+    return {
+      bills
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 /* ici les futurs styles du composant écrit avevc du scss */
---
+++ TP/jour17-vuejs/8-exercice-facture/src/components/BillTableRow.vue
@@ -0,0 +1,24 @@
+<template>
+  <tr v-if="bill">
+    <td>{{ bill.date }} - {{ bill.description }} - {{ bill.billnum }}</td>
+    <td>{{ bill.client.firstName }} {{ bill.client.lastName }}</td>
+    <td class="text-end">{{ bill.totalHT.toFixed(2) }} HT</td>
+    <td class="text-end">{{ bill.totalTTC.toFixed(2) }} TTC</td>
+    <td class="d-flex justify-content-end gap-2">
+      <button class="btn btn-outline-danger">
+        <i class="fa-solid fa-trash me-1" />
+        Supprimer
+      </button>
+      <button class="btn btn-outline-info">
+        <i class="fa-solid fa-pen me-1" />
+        Editer
+      </button>
+    </td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: ['bill']
+}
+</script>
